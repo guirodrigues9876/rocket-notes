dd6fcf66ff71c3829d6cda3ec65e3169b945df08
guilherme <guilherme121225@gmail.com>
2023-06-01 16:48:53 -0300

    Page home

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { ThemeProvider } from 'styled-components'; 
 import GlobalStyles from './styles/global';
-import { Details } from './pages/Details';
+import { Home } from './pages/Home';
 
 import theme from './styles/theme';
 
@@ -10,7 +10,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <Details />
+      <Home />
     </ThemeProvider>
   </React.StrictMode>,
 )
---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,47 @@
+import { FiPlus } from 'react-icons/fi';
+import { Container, Brand, Menu, Search, Content, NewNote } from  './styles';
+
+import { Header } from '../../components/Header';
+import { ButtonText } from '../../components/ButtonText';
+import { Input } from '../../components/Input';
+import { Note } from '../../components/Note';
+import { Section } from '../../components/Section';
+
+export function Home(){
+    return(
+        <Container>
+            <Brand>
+                <h1>Rocketnotes</h1>
+            </Brand>
+            <Header>
+
+            </Header>
+            <Menu>
+                <li><ButtonText title="Todos"/></li>
+                <li><ButtonText title="React" /></li>
+                <li><ButtonText title="Nodejs" /></li>
+
+            </Menu>
+            <Search>
+                <Input placeholder="Pesquisar pelo título" />
+            </Search>
+            <Content>
+                <Section title="minhas notas" >
+                    <Note data={{
+                        title: 'React', 
+                        tags: [
+                            {id: '1', name: 'react'},
+                            {id: '2', name: 'rocketseat'}
+                        ]
+                    }} 
+                />
+                </Section>
+            </Content>
+            <NewNote>
+                <FiPlus />
+                Criar nota
+            </NewNote>
+
+        </Container>
+    );
+}
