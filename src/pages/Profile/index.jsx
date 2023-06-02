64c42e323d35f878c524d6a1762a6f6916a59617
guilherme <guilherme121225@gmail.com>
2023-06-02 16:05:42 -0300

    feat: Profile

--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -1,6 +1,7 @@
-import { FiArrowLeft } from 'react-icons/fi';
-import { Container } from "./styles";
-
+import { FiArrowLeft, FiUser, FiMail, FiLock, FiCamera } from 'react-icons/fi';
+import { Container, Form, Avatar } from "./styles";
+import { Input } from "../../components/Input";
+import { Button } from "../../components/Button";
 
 export function Profile(){
     return(
@@ -10,6 +11,46 @@ export function Profile(){
                     < FiArrowLeft />
                 </a>
             </header>
+
+            <Form>
+                <Avatar>
+                    <img
+                        src="https://github.com/guirodrigues9876.png"
+                        alt="Foto do usuário"
+                    />
+
+                    <label htmlFor="avatar">
+                        <FiCamera />
+
+                        <input 
+                            id="avatar"
+                            type="file"
+                        />
+                    </label>
+                </Avatar>
+                <Input 
+                    placeholder="Nome"
+                    type="text"
+                    icon={FiUser}
+                />
+                <Input 
+                    placeholder="Email"
+                    type="text"
+                    icon={FiMail}
+                />
+                <Input 
+                    placeholder="Senha atual"
+                    type="password"
+                    icon={FiLock}
+                />
+                <Input 
+                    placeholder="Nova senha"
+                    type="password"
+                    icon={FiLock}
+                />
+
+                <Button title="Salvar" />
+            </Form>
         </Container>
     )
 }
